a5390c09565be33ef89cf0655fc7e5b47b16c720
bodwood <bodiewood@hotmail.com>
2022-11-28 20:47:30 -0800

    change class name to DoubleIt and add line for user to input a number. After user number is input, that number is doubled and returned

--- Hello.cs
+++ Hello.cs
@@ -1,9 +1,12 @@
 using System;
 
-class Program
+class DoubleIt
 {
   static void Main()
   {
-    Console.WriteLine("Hello World!");
+    Console.WriteLine("Enter a number to double: ");
+    int userNum = Convert.ToInt32(Console.ReadLine());
+    int doubledNum = userNum * 2;
+    Console.WriteLine(doubledNum);
   }
 }
