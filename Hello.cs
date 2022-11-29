7447bc50496c0959aaa7632c23bfe4c4638cfbfd
bodwood <bodiewood@hotmail.com>
2022-11-28 21:22:42 -0800

    change class name to SeeSharp and add a few lines of code to decide if a user is old enough to watch a rated R movie or not

--- Hello.cs
+++ Hello.cs
@@ -1,13 +1,24 @@
 using System;
 
-class DoubleIt
+class SeeSharp
 {
   static void Main()
   {
+    //Integers and Strings
     Console.WriteLine("Enter a number to double: ");
     string userNum = Console.ReadLine();
     int doubledNum = int.Parse(userNum) * 2; //int.Parse converts string to int
-    string userMessage = userNum + " doubled is " + doubledNum.ToString();
+    string userMessage = userNum + " doubled is " + doubledNum.ToString();  //.ToString() converts integers to a string
     Console.WriteLine(userMessage);
+    Console.WriteLine(" ");
+
+    //Branching
+    Console.WriteLine("Enter your age: ");
+    string userAge = Console.ReadLine();
+    if(int.Parse(userAge) >= 18){
+      Console.WriteLine("Welcome in! Here is your ticket!");
+    } else {
+      Console.WriteLine("You must be at least 18 years old to watch a rated R movie.");
+    }
   }
 }
